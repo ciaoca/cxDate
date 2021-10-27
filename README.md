dbd65adac8c48252454b13461145c3448aee4bfc
ciaoca <ciaoca@gmail.com>
2021-10-27 17:05:38 +0800

    feat v1.1

--- README.md
+++ README.md
@@ -3,79 +3,74 @@
 cxDate 是一个用于格式化日期的插件，通过自定义样式来格式化日期，支持处理字符串及时间戳格式的日期值。
 
 **版本：**
+* cxDate v1.1
 
-* cxDate v1.0
+Demo: http://ciaoca.github.io/cxDate/
 
-文档：http://code.ciaoca.com/javascript/cxDate/
 
-## 参数说明
-名称|默认值|说明
----|---|---
-style|null|必要参数，日期格式化的样式
-time|new Date()|可选参数，需要格式化的时间。默认使用当前时间。<br>可传入一个日期字符串，如："2014-03-20 20:35:00"、"Aug 9, 1995"、"3/29/2014"<br>可传入一个时间戳（距离 1970-1-1 00:00:00 相差的毫秒数）<br>注意：时间戳为毫秒数，PHP 的 time() 及 strtotime() 方法返回的为秒数，需乘以 1000 之后再进行处理。
 
-## data 属性参数
+## 使用方法
 
-字符|说明|返回值示例
----|---|---
-YY|年份，仅末尾 2 位数字|如：08 或 14
-YYYY|年份，完整 4 位数字|如：2008 或 2014
-M|月份，数字|1 到 12
-MM|月份，数字带前导零|01 到 12
-MMM|月份，缩写名称，文本|'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-MMMM|月份，完整名称，文本|'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
-D|月份中的第几天，数字|1 到 31
-DD|月份中的第几天，数字带前导零|01 到 31
-d|星期中的第几天，数字|0（表示周日）到 6（表示周六）
-dd|星期中的第几天，数字|1（表示周一）到 7（表示周日）
-ddd|星期中的第几天，缩写名称，文本|'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
-dddd|星期中的第几天，完整名称，文本|'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-H|小时，24 小时格式，数字|0 到 23
-HH|小时，24 小时格式，数字带前导零|00 到 23
-h|小时，12 小时格式，数字|1 到 12
-hh|小时，12 小时格式，数字带前导零|01 到 12
-m|分钟，数字|0 到 59
-mm|分钟，数字带前导零|00 到 59
-s|秒数，数字|0 到 59
-ss|秒数，数字带前导零|00 到 59
-A|大写的上午和下午值，文本|AM 或 PM
-a|小写的上午和下午值，文本|am 或 pm
-
-## 多语言配置
-
-### 需要在 cxdate.js 之前引入
+### 载入 JavaScript 文件
 
 ```html
-<script src="cxdate.languages.js"></script>
 <script src="cxdate.js"></script>
 ```
-
-### 配置文件说明
+### 调用 cxCalendar
 
 ```javascript
-cxDateLanguages = {
-  // 浏览器的语言，纯小写字母
-  'en' : {
-    // 月份名称缩写
-    monthAbbr : ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    // 月份完整名称
-    monthName : ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-    // 星期名称缩写
-    weekAbbr : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    // 星期完整名称
-    weekName : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-    // 上午、下午小写
-    amName : ['am', 'pm'],
-    // 上午、下午大写
-    AMName : ['AM', 'PM']
-  },
-  'language name' : {
-    monthAbbr : [...],
-    monthName : [...],
-    weekAbbr : [...],
-    weekName : [...],
-    amName : [...],
-    AMName : [...]
-  }
-};
+cxDate('YYYY-MM-DD');
+cxDate('YYYY-MM-DD', 1577808000000);
+cxDate('YYYY-MM-DD', '1/1/2020');
+
+// 自定义语言，完整配置可参考源码 language
+cxDate.setLanguage({
+  monthAbbr: [..],
+  monthName: [..],
+  weekAbbr: [..],
+  weekName: [..],
+  amName: [..],
+  AMName: [..]
+});
 ```
+
+
+## 参数说明
+
+名称 | 默认值 | 说明
+--- | --- | ---
+style | null | 必要参数，日期格式化的样式
+time | new Date() | 可选参数，需要格式化的时间，默认使用当前时间。<br>可传入日期字符串，如：'2014-03-20 20:35:00', '3/29/2014'<br>可传入时间戳（毫秒）
+
+
+
+## style 属性参数
+
+字符 | 说明 | 返回值示例
+--- | --- | ---
+YY | 年份，仅末尾 2 位数字 | 如：08 或 14
+YYYY | 年份，完整 4 位数字 | 如：2008 或 2014
+M | 月份 | 1 到 12
+MM | 月份，带前导零 | 01 到 12
+MMM | 月份，缩写名称 | 'Jan', 'Feb', ..
+MMMM | 月份，完整名称 | 'January', 'February', ..
+D | 月份中的第几天 | 1 到 31
+DD | 月份中的第几天，带前导零 | 01 到 31
+DDD | 年份中的第几天 | 1 到 366
+DDDD | 年份中的第几天，带前导零 | 001 到 366
+d | 星期的第几天 | 0（周日）到 6（周六）
+ddd | 星期的第几天，缩写名称 | 'Sun', 'Mon', ..
+dddd | 星期的第几天，完整名称 | 'Sunday', 'Monday', ..
+w | 年份中的第几周 | 1 到 53
+ww | 年份中的第几周，带前导零 | 01 到 53
+H | 小时，24 小时格式 | 0 到 23
+HH | 小时，24 小时格式，带前导零 | 00 到 23
+h | 小时，12 小时格式 | 1 到 12
+hh | 小时，12 小时格式，带前导零 | 01 到 12
+m | 分钟 | 0 到 59
+mm | 分钟，带前导零 | 00 到 59
+s | 秒数 | 0 到 59
+ss | 秒数，带前导零 | 00 到 59
+A | 大写的上午或下午 | AM 或 PM
+a | 小写的上午或下午 | am 或 pm
+
---
+++ js/cxdate.js
@@ -0,0 +1,212 @@
+/*!
+ * cxDate
+ * 
+ * @version 1.1
+ * @author ciaoca
+ * @email ciaoca@gmail.com
+ * @site https://github.com/ciaoca/cxDate
+ * @license Released under the MIT license
+ */
+(function(window, undefined) {
+  var plugins = {
+    language: {
+      // 月份名称缩写
+      monthAbbr: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+      // 月份完整名称
+      monthName: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+      // 星期名称缩写
+      weekAbbr: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
+      // 星期完整名称
+      weekName: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+      // 上午、下午小写
+      amName: ['am','pm'],
+      // 上午、下午大写
+      AMName: ['AM','PM']
+    },
+    reg: {
+      isYear: /^\d{4}$/,
+      isTime: /^\d{1,2}(\:\d{1,2}){1,2}$/
+    },
+    isDateObject: function(obj) {
+      return Object.prototype.toString.call(obj) === '[object Date]' && typeof obj.getTime === 'function' && !isNaN(obj.getTime());
+    },
+  };
+
+  // 补充前置零
+  plugins.fillLeadZero = function(value, num) {
+    var str = String(value);
+
+    if (str.length < num) {
+      str = Array(num - str.length).fill(0).join('') + value;
+    };
+
+    return str;
+  };
+
+  // 解析日期
+  plugins.parseDate = function(value) {
+    var self = this;
+    var theDate = new Date();
+    var tags;
+
+    if (self.reg.isYear.test(value)) {
+      theDate.setFullYear(parseInt(value, 10));
+
+    } else if (/^\d+$/.test(value) || (typeof value === 'number' && isFinite(value))) {
+      theDate.setTime(parseInt(value, 10));
+
+    } else if (typeof value === 'string') {
+      if (self.reg.isTime.test(value)) {
+        tags = value.split(':');
+
+        if (tags.length === 2) {
+          tags.push(0);
+        };
+
+        theDate.setHours(tags[0], tags[1], tags[2]);
+
+      } else {
+        theDate = new Date(value);
+      };
+    };
+
+    if (!self.isDateObject(theDate) || isNaN(theDate.getTime())) {
+      theDate = new Date();
+    };
+
+    return theDate;
+  };
+
+  // 获取周数
+  plugins.getWeekNum = function(dateObj) {
+    var self = this;
+    var curTime = dateObj.getTime();
+    var yearFirstDate = new Date(dateObj.getFullYear(), 0, 1, 0, 0, 0, 0);
+    var weekFirstTime = yearFirstDate.getTime();
+    var weekDay = yearFirstDate.getDay();
+    var weekNum = 0;
+
+    if (weekDay === 0) {
+      weekDay = 7;
+    };
+
+    var weekOffset = weekDay > 4 ? -1 : 0;
+
+    if (weekDay > 4) {
+      weekFirstTime += (8 - weekDay) * 86400000;
+    } else {
+      weekFirstTime += (1 - weekDay) * 86400000;
+    };
+
+    if (curTime < weekFirstTime) {
+      weekNum = self.getWeekNum(new Date(dateObj.getFullYear() - 1, 11, 31));
+    } else {
+      weekNum = Math.floor((curTime - weekFirstTime) / 86400000) + 1;
+      weekNum = Math.ceil(weekNum / 7);
+    };
+
+    return weekNum;
+  };
+
+  plugins.getAttr = function(date) {
+    var self = this;
+    var attr = {};
+    var timestamp = date.getTime();
+
+    attr.YYYY = date.getFullYear();
+    attr.YY = String(attr.YYYY).slice(-2);
+
+    var yearFirstDate = new Date(attr.YYYY, 0, 1, 0, 0, 0, 0);
+    var yearFirstTime = yearFirstDate.getTime();
+
+    attr.M = date.getMonth();
+    attr.MMM = self.language.monthAbbr[attr.M];
+    attr.MMMM = self.language.monthName[attr.M];
+    attr.M += 1;
+    attr.MM = self.fillLeadZero(attr.M, 2);
+
+    attr.D = date.getDate();
+    attr.DD = self.fillLeadZero(attr.D, 2);
+    attr.DDD = Math.floor((timestamp - yearFirstTime) / 86400000) + 1;
+    attr.DDDD = self.fillLeadZero(attr.DDD, 3);
+
+    attr.d = date.getDay();
+    attr.ddd = self.language.weekAbbr[attr.d];
+    attr.dddd = self.language.weekName[attr.d];
+
+    attr.w = self.getWeekNum(date);
+    attr.ww = self.fillLeadZero(attr.w, 2);
+
+    attr.H = date.getHours();
+    attr.HH = self.fillLeadZero(attr.H, 2);
+    attr.h = (attr.H > 12) ? attr.H - 12: attr.H;
+    attr.hh = self.fillLeadZero(attr.h, 2);
+    attr.m = date.getMinutes();
+    attr.mm = self.fillLeadZero(attr.m, 2);
+    attr.s = date.getSeconds();
+    attr.ss = self.fillLeadZero(attr.s, 2);
+
+    if (attr.H > 12) {
+      attr.A = self.language.AMName[1];
+      attr.a = self.language.amName[1];
+    } else {
+      attr.A = self.language.AMName[0];
+      attr.a = self.language.amName[0];
+    };
+
+    return attr;
+  };
+
+  plugins.format = function(attr, style) {
+    var str = style;
+    var keys = ['YYYY','YY','MMMM','MMM','MM','M','DDDD','DDD','DD','D','dddd','ddd','d','ww','w','HH','H','hh','h','mm','m','ss','s','A','a'];
+    var reg = new RegExp('(' + keys.join('|') + ')', 'g');
+
+    // 转义边界符号
+    str = str.replace(/([\{\}])/g, '\\$1');
+
+    // 转义关键词
+    str = str.replace(reg, function(match, p1) {
+      return '{{' + p1 + '}}';
+    });
+
+    // 还原转义字符
+    str = str.replace(/\\\{\{(.)\}\}/g, '$1');
+
+    // 替换关键词
+    for (var i = keys.length - 1; i >= 0; i--) {
+      str = str.replace(new RegExp('{{' + keys[i] + '}}', 'g'), attr[keys[i]]);
+    };
+
+    // 还原转义内容
+    str = str.replace(/\\(.)/g, '$1');
+
+    return str;
+  };
+
+  plugins.decode = function(style, time) {
+    var self = this;
+
+    if (typeof style !== 'string') {
+      console.warn('[cxDate] Missing parameters.');
+      return false;
+    };
+
+    time = self.parseDate(time);
+
+    var attr = self.getAttr(time);
+    var text = self.format(attr, style);
+
+    return text;
+  };
+
+  var cxDate = function(style, time) {
+    return plugins.decode.apply(plugins, arguments);
+  };
+
+  cxDate.setLanguage = function(options) {
+    Object.assign(plugins.language, options);
+  };
+
+  window.cxDate = cxDate;
+})(window);
